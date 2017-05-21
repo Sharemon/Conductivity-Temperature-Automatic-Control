59097f1f3106e918dc1ee3c9ef12e1bd12e829f8
sharemon <sharemon@163.com>
2017-05-21 23:19:32 +0800

    update uart test project and move remote control project to JiLiangYuan repository

--- UART Test Program/串口测试程序/Form1.cs
+++ UART Test Program/串口测试程序/Form1.cs
@@ -28,6 +28,9 @@ namespace 串口测试程序
 
         private void button1_Click(object sender, EventArgs e)
         {
+            float[] parameters = new float[7];
+
+
             this.button1.Enabled = false;
             sp.PortName = "COM" + COM.Text;
             sp.Open();
@@ -44,52 +47,54 @@ namespace 串口测试程序
                     {
                         if (str[i] == '@')  //找到@作为标志位
                         {
-                                switch (str[i + 3])
-                                {
-                                    case 'R':
-                                        switch(str[i+4])
-                                        {
-                                            case 'H':
-                                                Random rn = new Random();
-                                                double tempera = 25+int.Parse(this.textBox1.Text)*rn.NextDouble()/1000;
-                                                sp.WriteLine("@35RH"+tempera.ToString("0.0000")+":");
-                                                TxtSend.Text = "@35RH" + tempera.ToString("0.0000") + ":";
-                                                break;
-                                            case 'I':
-                                                 sp.WriteLine("@35RI98:");
-                                                break;
-                                            case 'A':
-                                                sp.WriteLine("@35RA9.098:");
-                                                break;
-                                            case 'B':
-                                                sp.WriteLine("@35RB9.098:");
-                                                break;
-                                            case 'C':
-                                                sp.WriteLine("@35RC9.098:");
-                                                break;
-                                            case 'D':
-                                                sp.WriteLine("@35RD9:");
-                                                break;
-                                            case 'E':
-                                                sp.WriteLine("@35RE9:");
-                                                break;
-                                            case 'F':
-                                                sp.WriteLine("@35RF9:");
-                                                break;
-                                            case 'G':
-                                                sp.WriteLine("@35RG9:");
-                                                break;
-                                            default: MessageBox.Show(str); break;
-                                        }
-                                        break;
-                                        case 'W':
-                                            //sp.WriteLine(str.Substring(0,5) + ":");
-                                            sp.WriteLine("@35WA:");
-                                            TxtSend.Text = str.Substring(0, 5) + ":";
-                                            this.Text = str;
+                            switch (str[i + 3])
+                            {
+                                case 'R':
+                                    switch (str[i + 4])
+                                    {
+                                        case 'H':
+                                            Random rn = new Random();
+                                            double tempera = 25 + int.Parse(this.textBox1.Text) * rn.NextDouble() / 1000;
+                                            sp.WriteLine("@35RH" + tempera.ToString("0.0000") + ":");
+                                            TxtSend.Text = "@35RH" + tempera.ToString("0.0000") + ":";
                                             break;
-                                    default: MessageBox.Show(str); break;
-                                }
+                                        case 'I':
+                                            Random rn2 = new Random();
+                                            double power = 50 + 20 * (rn2.NextDouble() - 0.5);
+                                            sp.WriteLine("@35RI" + power.ToString("0") + ":");
+                                            break;
+                                        case 'A':
+                                            sp.WriteLine("@35RA" + parameters[0].ToString("0.000") + ":");
+                                            break;
+                                        case 'B':
+                                            sp.WriteLine("@35RB" + parameters[1].ToString("0.000") + ":");
+                                            break;
+                                        case 'C':
+                                            sp.WriteLine("@35RC" + parameters[2].ToString("0.000") + ":");
+                                            break;
+                                        case 'D':
+                                            sp.WriteLine("@35RD" + parameters[3].ToString("0.000") + ":");
+                                            break;
+                                        case 'E':
+                                            sp.WriteLine("@35RE" + parameters[4].ToString("0.000") + ":");
+                                            break;
+                                        case 'F':
+                                            sp.WriteLine("@35RF" + parameters[5].ToString("0.000") + ":");
+                                            break;
+                                        case 'G':
+                                            sp.WriteLine("@35RG" + parameters[6].ToString("0.000") + ":");
+                                            break;
+                                        default: MessageBox.Show(str); break;
+                                    }
+                                    break;
+                                case 'W':
+                                    float value = float.Parse(str.Remove(0, 5));
+                                    parameters[str[i + 4] - 'A'] = value;
+                                    sp.WriteLine(str.Substring(0, 5) + ":");
+                                    TxtSend.Text = parameters[str[i + 4] - 'A'].ToString("0.000");
+                                    break;
+                                default: MessageBox.Show(str); break;
+                            }
                         }
                     }
                 }
